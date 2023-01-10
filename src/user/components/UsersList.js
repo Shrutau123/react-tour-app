0046156a62bcc2c8b3443586ecf9e315f9be5299
Shrutau123 <shrutauchourasia@gmail.com>
2023-01-10 16:16:50 +0530

    UI Done

--- src/user/components/UsersList.js
+++ src/user/components/UsersList.js
@@ -1,31 +1,31 @@
-import React from "react";
-import "./UsersList.css";
-import UserItem from "./UserItem";
-import Card from "../../shared/components/UiElements/Card/Card";
+import React from 'react';
 
-const UsersList = (props) => {
+import UserItem from './UserItem';
+import Card from '../../shared/components/UiElements/Card';
+import './UsersList.css';
+
+const UsersList = props => {
   if (props.items.length === 0) {
     return (
       <div className="center">
         <Card>
-          <h2>No users Found</h2>
+          <h2>No users found.</h2>
         </Card>
       </div>
     );
   }
+
   return (
     <ul className="users-list">
-      {props.items.map((user) => {
-        return (
-          <UserItem
-            key={user.id}
-            id={user.id}
-            image={user.image}
-            name={user.name}
-            placeCount={user.places}
-          />
-        );
-      })}
+      {props.items.map(user => (
+        <UserItem
+          key={user.id}
+          id={user.id}
+          image={user.image}
+          name={user.name}
+          placeCount={user.places}
+        />
+      ))}
     </ul>
   );
 };
